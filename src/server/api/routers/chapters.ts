850e13d2095173208f049ba0d74c2db54f21ad3f
Alexandro Tapia <zpaceway@gmail.com>
2023-01-27 18:58:04 -0500

    include published at field

--- src/server/api/routers/chapters.ts
+++ src/server/api/routers/chapters.ts
@@ -4,7 +4,13 @@ export const chaptersRouter = createTRPCRouter({
   getChapters: protectedProcedure.query(async ({ ctx }) => {
     const chapters = await ctx.prisma.chapter.findMany({
       include: {
-        lessons: true,
+        lessons: {
+          where: {
+            published_at: {
+              not: null,
+            },
+          },
+        },
       },
     });
 
