1853068f0489cdb2c261886adaa6ce1385e1449f
Alexandro Tapia <zpaceway@gmail.com>
2023-02-04 02:09:24 -0500

    fix minor styling issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -344,16 +344,16 @@ const Home: NextPage = () => {
             </div>
             <div>
               <div
-                className={`flex flex-col ${
+                className={`flex flex-col gap-6 p-6 ${
                   isNavBarOpened ? "xl:flex-row" : "lg:flex-row"
                 }`}
               >
                 {selectedLesson.html && (
-                  <div className="flex w-full flex-row p-6">
+                  <div className="flex w-full flex-row">
                     <LessonHTML html={selectedLesson.html} />
                   </div>
                 )}
-                <div className="flex w-full max-w-[480px] flex-col p-6">
+                <div className="flex w-full max-w-[480px] flex-col">
                   <div className="rounded-lg border p-6">
                     <div className="flex flex-col gap-4">
                       {comments.map((comment) => (
